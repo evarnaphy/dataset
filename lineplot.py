ddac6c166c1f873b117b29bd847846b26b7b1f1e
evarnaphy <evarnaphy@gmail.com>
2024-10-01 11:53:03 +0300

    Remove virtual environment from tracking by git

---
+++ lineplot.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import plotly.express as px  
+import dash  
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+
+# ------------------------------------------------------------------------------
+# Import and clean data (importing csv into pandas)
+#df = pd.read_csv("Dataset/intro_bees.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/LubangaD/beesurveydashboard/refs/heads/main/Dataset/intro_bees.csv")
+
+df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
+df.reset_index(inplace=True)
+print(df[:5])
+
+bee_killers = ["Disease", "Other", "Pesticides", "Pests_excl_Varroa", "Unknown", "Varroa_mites"]
+print(bee_killers)
+
+# ------------------------------------------------------------------------------
+# App layout
+app.layout = html.Div([
+
+    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
+
+    dcc.Dropdown(id="slct_impact",
+                 options=[{"label": x, "value":x} for x in bee_killers],
+                 value="Pesticides",
+                 multi=False,
+                 style={'width': "40%"}
+                 ),
+
+    html.Div(id='output_container', children=[]),
+    html.Br(),
+
+    dcc.Graph(id='my_bee_map', figure={})
+
+])
+# ------------------------------------------------------------------------------
+# Connect the Plotly graphs with Dash Components
+@app.callback(
+    [Output(component_id='output_container', component_property='children'),
+     Output(component_id='my_bee_map', component_property='figure')],
+    [Input(component_id='slct_impact', component_property='value')]
+)
+def update_graph(option_slctd):
+    print(option_slctd)
+    print(type(option_slctd))
+
+    container = "The bee-killer chosen by user was: {}".format(option_slctd)
+
+    dff = df.copy()
+    dff = dff[dff["Affected by"] == option_slctd]
+    dff = dff[(dff["State"] == "Idaho") | (dff["State"] == "New York") | (dff["State"] == "New Mexico")]
+
+    fig = px.line(
+        data_frame=dff,
+        x='Year',
+        y='Pct of Colonies Impacted',
+        color='State',
+        template='plotly_dark'
+    )
+
+    return container, fig
+
+
+# ------------------------------------------------------------------------------
+if __name__ == '_main_':
+    app.run_server(debug=True)
